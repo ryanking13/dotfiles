231f6d9f27a9613c7e2926697cc0a1fa8dfa5db6
ryanking13 <def6488@gmail.com>
2018-05-03 14:53:35 +0900

    update install files

---
+++ after_install.sh
@@ -0,0 +1,3 @@
+# run with zsh
+
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
--- install.sh
+++ install.sh
@@ -1,3 +1,16 @@
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ~/.vimrc
 cp .zshrc ~/.zshrc
+
+# other utilities
+cd ~
+mkdir _utils
+cd _utils
+
+# bat
+echo "[*] INSTALLING bat"
+wget https://github.com/sharkdp/bat/releases/download/v0.2.2/bat-v0.2.2-x86_64-unknown-linux-gnu.tar.gz
+tar xvf bat-v0.2.2-x86_64-unknown-linux-gnu.tar.gz
+rm bat-v0.2.2-x86_64-unknown-linux-gnu.tar.gz
+mv bat-v0.2.2-x86_64-unknown-linux-gnu _bat
