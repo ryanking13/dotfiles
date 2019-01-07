156db3d667de8caf51018078d72856741be2e2ee
ryanking13 <def6488@gmail.com>
2019-01-07 12:06:35 +0900

    add fzf

--- .zshrc
+++ .zshrc
@@ -75,7 +75,7 @@ source $ZSH/oh-my-zsh.sh
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
 #
-export PATH=$PATH':'$HOME'/_utils'
+export PATH=$PATH':'$HOME'/.utils'
 
 alias gr="grep -r -n"
 alias lst="ls -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'"
--- install.sh
+++ install.sh
@@ -15,15 +15,20 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # other utilities
 cd ~
-if [ ! -e _utils ]; then
-    mkdir _utils
+if [ ! -e .utils ]; then
+    mkdir .utils
 fi
-cd _utils
+cd .utils
 
 # bat
-echo "[*] INSTALLING bat"
+echo "[*] Installing: bat"
 wget https://github.com/sharkdp/bat/releases/download/v0.2.2/bat-v0.2.2-x86_64-unknown-linux-gnu.tar.gz
 tar xvf bat-v0.2.2-x86_64-unknown-linux-gnu.tar.gz
 rm bat-v0.2.2-x86_64-unknown-linux-gnu.tar.gz
 mv bat-v0.2.2-x86_64-unknown-linux-gnu/bat bat
 rm -rf bat-v0.2.2-x86_64-unknown-linux-gnu
+
+# fzf
+echo "[*] Installing: fzf"
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+yes | ~/.fzf/install
